b15dd6445a3cdefb349b778f6a3dafb60c4e6b8f
Alex <alex.gyumushyan@gmail.com>
2021-06-04 16:17:20 -0400

    working on fetching data from api

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,48 +1,49 @@
+// practicing axios fetch API
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import Axios from 'axios'
 
 
 
 const Search = () => {
-    
-    const url = 'http://localhost:3000/getQuoteRoute/Tech';
-    const [product, setProduct] = useState(null);
-
-  
-
-    useEffect(() => {
-        axios.get(url)
-            .then(response =>{
-                setProduct(response.data);
+    const [count, setcount] = useState(0);
+    const [state, setstate] = useState(null);
+    /*
+    const getFromApi = () =>{
+        Axios.get('http://localhost:3001/getQuoteRoute/Tech')
+            .then((response) => {
+                console.log(response);
+                const data = [];
+                
+                setstate(response.data.title);
             })
-    }, [url])
-    
-    console.log(product[0]);
-    
-    
-    if (product){
+    } 
+    */
+        
+    useEffect(async () => {
+        /*
+        Axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            .then((response) =>{
+                console.log(response);
+                setstate(response);
+            })
+        */
 
-        return (
-            <div style=
-            {{
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center',
-                height: '90vh'
-            }}
-        >
-        <h1>{product[0].Stocks[0]} </h1>
-            
-        </div>
-        )
-    }
-
+        const response = await fetch('http://localhost:3001/getQuoteRoute/Tech');
+        const data = await response.json();
+        const stocks = data[0].Stocks;
+        console.log(stocks[0]);
+        setstate(stocks[0]);
+    }, []);    
+        
     return (
         <div>
-
+            <h1> You clicked me {count} times </h1>
+            <button onClick={() => setcount(count+1)}>increase the count </button>
+        
+            {state && <div> {state.price} </div>}
+        
         </div>
     )
-    
 }
 
 export default Search

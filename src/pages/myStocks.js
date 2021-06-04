03377b139e03a7e58e8ed917470685ad4cbf2d4c
Alex <alex.gyumushyan@gmail.com>
2021-06-04 17:58:35 -0400

    issues with useState and useEffect hooks

--- src/pages/myStocks.js
+++ src/pages/myStocks.js
@@ -13,14 +13,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-/*
-const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-});
-*/
-
 function createData(ticker, name, price, dailyChange) {
     return { ticker, name, price, dailyChange };
 }
@@ -33,51 +25,36 @@ const data = [
     createData('tsla', 'Tesla', 680, -23.30),
 ];
 
+
 const stockChart = () => {
-    /*
-    const url = 'http://localhost:3000/getQuoteRoute/Tech';
-    const [product, setProduct] = useState(null);
-
-    axios.get(url)
-        .then(response =>{
-            setProduct(response.data);
-        })
-
-    useEffect(() => {
-        axios.get(url)
-        .then(response =>{
-            setProduct(response.data);
-        })
-    }, [url])
-    
-    */
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Ticker</TableCell>
-                        <TableCell>Company Name</TableCell>
-                        <TableCell>Price</TableCell>
-                        <TableCell>Daily Change</TableCell>
-                    </TableRow>
-                </TableHead>
-
-                <TableBody>
-                    {data.map((element) => (
-                        <TableRow key ={element.ticker}>
-                            <TableCell component ="th" scope="row">
-                                {element.ticker}
-                            </TableCell>
-                            <TableCell alight="right">{element.name}</TableCell>
-                            <TableCell alight="right">{element.price}</TableCell>
-                            <TableCell alight="right">{element.dailyChange}</TableCell>
+        <div>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ticker</TableCell>
+                            <TableCell>Company Name</TableCell>
+                            <TableCell>Price</TableCell>
+                            <TableCell>Daily Change</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
+                    </TableHead>
 
-            </Table>
-        </TableContainer>
+                    <TableBody>
+                        {data.map((element) => (
+                            <TableRow key ={element.ticker}>
+                                <TableCell component ="th" scope="row">
+                                    {element.ticker}
+                                </TableCell>
+                                <TableCell alight="right">{element.name}</TableCell>
+                                <TableCell alight="right">{element.price}</TableCell>
+                                <TableCell alight="right">{element.dailyChange}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,38 +1,28 @@
 // practicing axios fetch API
 import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
+import axios from 'axios';
 
+// import stuff for the material design import Table from '@material-ui/core/Table';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 const Search = () => {
-    const [count, setcount] = useState(0);
+    const [count, setcount] = useState([]);
     const [state, setstate] = useState(null);
-    /*
-    const getFromApi = () =>{
-        Axios.get('http://localhost:3001/getQuoteRoute/Tech')
-            .then((response) => {
-                console.log(response);
-                const data = [];
-                
-                setstate(response.data.title);
-            })
-    } 
-    */
+    
         
     useEffect(async () => {
-        /*
-        Axios.get('https://jsonplaceholder.typicode.com/todos/1')
-            .then((response) =>{
-                console.log(response);
-                setstate(response);
-            })
-        */
-
-        const response = await fetch('http://localhost:3001/getQuoteRoute/Tech');
-        const data = await response.json();
+        const response = await axios.get('http://localhost:3001/getQuoteRoute/Tech');
+        const data = await response.data;
         const stocks = data[0].Stocks;
-        console.log(stocks[0]);
-        setstate(stocks[0]);
+        console.log(stocks);
+        setstate(stocks);
     }, []);    
         
     return (
@@ -40,7 +30,33 @@ const Search = () => {
             <h1> You clicked me {count} times </h1>
             <button onClick={() => setcount(count+1)}>increase the count </button>
         
-            {state && <div> {state.price} </div>}
+            {state && <div> {state[0].price} </div>}
+
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ticker</TableCell>
+                            <TableCell>Company Name</TableCell>
+                            <TableCell>Price</TableCell>
+                            <TableCell>Daily Change</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {state.map((element) => ( // current error is that this is null when executing
+                            <TableRow key ={element.ticker}>
+                                <TableCell component ="th" scope="row">
+                                    {element.ticker}
+                                </TableCell>
+                                <TableCell alight="right">{element.name}</TableCell>
+                                <TableCell alight="right">{element.price}</TableCell>
+                                <TableCell alight="right">{element.dailyChange}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         
         </div>
     )

604faba738052495f891185273f63ff17487d17c
Alex <alex.gyumushyan@gmail.com>
2021-06-07 12:05:25 -0400

    fixed the issue with hooks

--- src/pages/search.js
+++ src/pages/search.js
@@ -13,8 +13,8 @@ import Paper from '@material-ui/core/Paper';
 
 
 const Search = () => {
-    const [count, setcount] = useState([]);
-    const [state, setstate] = useState(null);
+    const [count, setcount] = useState(0);
+    const [data, setData] = useState(null);
     
         
     useEffect(async () => {
@@ -22,41 +22,43 @@ const Search = () => {
         const data = await response.data;
         const stocks = data[0].Stocks;
         console.log(stocks);
-        setstate(stocks);
+        setData(stocks);
     }, []);    
+    
+    //{data && <div> {data.price} hello there </div>}
         
     return (
         <div>
             <h1> You clicked me {count} times </h1>
             <button onClick={() => setcount(count+1)}>increase the count </button>
-        
-            {state && <div> {state[0].price} </div>}
-
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Ticker</TableCell>
-                            <TableCell>Company Name</TableCell>
-                            <TableCell>Price</TableCell>
-                            <TableCell>Daily Change</TableCell>
-                        </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                        {state.map((element) => ( // current error is that this is null when executing
-                            <TableRow key ={element.ticker}>
-                                <TableCell component ="th" scope="row">
-                                    {element.ticker}
-                                </TableCell>
-                                <TableCell alight="right">{element.name}</TableCell>
-                                <TableCell alight="right">{element.price}</TableCell>
-                                <TableCell alight="right">{element.dailyChange}</TableCell>
+            
+            {data && <div> {data[0].price}
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Ticker</TableCell>
+                                <TableCell>Company Name</TableCell>
+                                <TableCell>Price</TableCell>
+                                <TableCell>Daily Change</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+
+                        <TableBody>
+                            {data.map((element) => ( // current error is that this is null when executing
+                                <TableRow key ={element.ticker}>
+                                    <TableCell component ="th" scope="row">
+                                        {element.ticker}
+                                    </TableCell>
+                                    <TableCell alight="right">{element.companyName}</TableCell>
+                                    <TableCell alight="right">{element.price}</TableCell>
+                                    <TableCell alight="right">{element.dailyChange}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>}
         
         </div>
     )

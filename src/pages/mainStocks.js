be7a62ee328e5819c99e8fb2a88249f813198c2d
Alex <alex.gyumushyan@gmail.com>
2021-06-11 22:18:52 -0400

    fixed the line graph

--- src/pages/mainStocks.js
+++ src/pages/mainStocks.js
@@ -2,18 +2,90 @@
 import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios';
 
+
 //import SearchBar from "material-ui-search-bar";
-import data1 from '../pages/SP500Companies.json';
+import sp500Data from '../pages/SP500Companies.json';
+
+// import chart framework
+import { Line } from 'react-chartjs-2';
+
 
 //YOUTUBE TUTORIAL: https://www.youtube.com/watch?v=i1uhccATDcY
 // also this: https://github.com/LetVDevelop/searchautocomplete/blob/AutoComplete/src/Search/AutoComplete.jsx
 
 const MainStocks = () => {
-    
+    /*
+    const data = {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [
+            {
+                label: 'price',
+                data: [12, 19, 3, 5, 2, 3],
+                fill: false,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgba(255, 99, 132, 0.2)',
+            },
+        ],
+    };
+    */
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                    beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
+
     const [searchtext, setSearchtext] = useState("");
     const [suggest, setSuggest] = useState([]);
     const [resfound, setResfound] = useState(true);
+
+    const [chosen , setChosen] = useState("");
+    const [chosenData, setChosenData] = useState([]);
     
+    useEffect(() => {
+        console.log('wtf');
+        async function fetchData(){
+            console.log(chosen);
+            var url = 'https://cloud.iexapis.com/stable/stock/' + chosen + '/chart/1m?token=pk_6591e91f712e4ab7b7e1eac1af451489&chartLast=25&chartCloseOnly=true';
+            const response = await axios.get(url);
+            const main = await response.data;
+            
+            // THIS WHOLE AREA IS COMPLETELY BROKEN  
+            var labels1 = [];
+            var dataSet1 = [];
+            for (var i = main.length -1; i >= 0; i--){
+                labels1.push(main[i].date);
+                dataSet1.push(main[i].close);
+            }
+            
+            const priceHistory = {
+                labels: labels1,
+                datasets: [
+                    {
+                        label: 'price',
+                        data: dataSet1,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgba(255, 99, 132, 0.2)',
+                    },
+                ],
+            };
+            console.log(priceHistory);
+            
+            
+            
+            setChosenData(priceHistory);
+            //console.log(chosenData);
+        }
+
+        fetchData();
+    }, [chosen]);
+    
+
     function filterValid(item, searchval){
         
         if (item.Name.toLowerCase().includes(searchval.toString())) {
@@ -31,15 +103,11 @@ const MainStocks = () => {
         let searchval = e.target.value;
         let suggestion = [];
         if (searchval.length > 0) {
-            console.log(searchval);
-            suggestion = data1
+            //console.log(searchval);
+            suggestion = sp500Data
                 .sort()
-                .filter(
-                        //x => x.Symbol.toString().toLowerCase() == searchval.toLowerCase() || x.Name.toLowerCase() == searchval.toLowerCase()
-                        //(e) => e.toString().toLowerCase().includes(searchval.toLowerCase())
-                        x => filterValid(x, searchval)
-                    );
-            console.log(suggestion);
+                .filter(x => filterValid(x, searchval));
+            //console.log(suggestion);
             setResfound(suggestion.length !== 0 ? true : false);
         }
         setSuggest(suggestion);
@@ -47,22 +115,9 @@ const MainStocks = () => {
     };
    
     const suggestedText = (value) => {
-        console.log(value);
         setSearchtext(value);
         setSuggest([]);
-
-
-        /*
-        {suggest.map((item, index) => {
-            return (
-            <div key={index}>
-                <li onClick={() => suggestedText(item)}>{item}</li>
-                {index !== suggest.length - 1 && <hr />}
-            </div>
-            );
-        })}
-
-        */
+        setChosen(value);
     };
 
     const getSuggestions = () => {
@@ -72,7 +127,6 @@ const MainStocks = () => {
     
         return (
             <ul>
-                {console.log(suggest)}
                 {suggest.slice(0,10).map((item, index) => {
                     return (
                         <li onClick={() => suggestedText(item.Symbol.toString())}>{item.Name.toString()}</li>
@@ -85,6 +139,17 @@ const MainStocks = () => {
     };
 
 
+    const createChart = () => {
+        if (chosen == ""){
+            return <p>No Chart</p>;
+        } else {
+            console.log("draw chart here");
+            // create a chart here 
+            //var url = process.env.REACT_APP_API_URL1 + 'tsla' + process.env.REACT_APP_API_URL2; 
+        
+            return (<Line data={chosenData} options={options} />);
+        }
+    }
 
 
     return (
@@ -97,7 +162,7 @@ const MainStocks = () => {
                 onChange={handleChange}
             />
             {getSuggestions()}
-            
+            {createChart()}
         </div>
     )
 }
